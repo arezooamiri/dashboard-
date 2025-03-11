cd0c2a70799c4de0b5b3efed9dbe0ab31a663fca
Arezoo <arezooayoblo@gmail.com>
2025-03-11 14:41:38 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-red-600'>
+          hi there
+      </div>
+        
+    </>
+  )
+}
+
+export default App
